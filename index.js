1222a63cd0c7aae6847e74fac473ca0b44334416
Nate Miller <natekmiller96@gmail.com>
2018-09-19 03:58:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ function scuberGreetingForFeet(someValue) {
 function ternaryCheckCity(city){
   let result;
   city === "NYC" ? (result = "OK sounds good.") : (isAdult = "No go.");
+  return result
 }
