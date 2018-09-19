0d15ee28ede08b639f2ebd6b1e89e5128812815c
Nate Miller <natekmiller96@gmail.com>
2018-09-19 03:52:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function scuberGreetingForFeet(someValue) {
   if (someValue < 400){
     result = "This one is on me!"
   } else if (someValue > 400 && someValue < 2500){
-    result = 30
+    result = 'I will gladly take your thirty bucks.'
   } else {
-    result = "No"
+    result = "No can do."
   }
   return result
 }
